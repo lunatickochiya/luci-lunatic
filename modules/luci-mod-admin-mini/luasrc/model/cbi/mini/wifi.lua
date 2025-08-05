ff344e1d9105f13453d7b57d9a2e9169e741eb26
benboguan <nanchuci023@gmail.com>
2025-08-05 23:59:07 +0800

    Revert "luci: mt_dbdc luci add ConnectedTime support"
    This reverts commit 056e14cd956d4c9a270421ede9c6b862393fa8e2.

--- modules/luci-mod-admin-mini/luasrc/model/cbi/mini/wifi.lua
+++ modules/luci-mod-admin-mini/luasrc/model/cbi/mini/wifi.lua
@@ -334,7 +334,7 @@ if hwtype == "mac80211" then
 end
 
 
-if hwtype == "broadcom" or hwtype == "mt_dbdc" then
+if hwtype == "broadcom" then
 	iso = s:option(Flag, "isolate", translate("AP-Isolation"), translate("Prevents Client to Client communication"))
 	iso.rmempty = true
 	iso:depends("mode", "ap")
